94b160e97f438c531050e329c96d21eaa317b4a6
a-ns <exmann94@gmail.com>
2016-09-06 13:09:29 -0500

    changed template: added exec and freopen, seems to work consistent

--- Project1/forkTemplate.c
+++ Project1/forkTemplate.c
@@ -6,23 +6,28 @@
 
 int main () {
   int i;
+  int status = 0;
+  char *output = "output.txt"; // outputRedirect from shell
   int n = 5; // this is read in as a paramter from shell
-  pid_t children[n];
-  for (i = 0; i < n; i++){
+  pid_t children[n], wpid;
+  for (i = 0; i < n; ++i){
     children[i] = fork();
-    if (children[i] == 0) //so we don't spawn 2^n - 1 processes, breaks out of the loop in child processes
-      break;
+    if (children[i] == -1){
+      exit(-1);
+    }
+    else if (children[i] == 0) {
+      //child
+      //call the exec function here i guess
+      printf ("i : %i\n", i);
+      printf("Child process : %i\n", getpid());
+      freopen (output, "a", stdout);
+      if (execl("/usr/bin/ls", "ls", "-l", NULL) == -1)
+        printf("exec fail\n");
+        exit(0);
+    }
   }
-  if (children[i] == 0) {
-    //child
-    //call the exec function here i guess
-    printf ("i : %i\n", i);
-    printf("Child process : %i\n", getpid());
-  }
-  else {
     //parent, waits for all other process to finish
     printf ("Parent process : %i\n", getpid());
-    wait(NULL);
-  }
+    while((wpid = wait(&status)) > 0);
   return 0;
 }
